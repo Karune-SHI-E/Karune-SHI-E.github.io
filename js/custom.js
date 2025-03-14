379ec8b5f80b9445b8734abe2b3708097e0a59f0
Karune-SHI-E <miku5201314520@gmail.com>
2025-03-14 16:23:33 +0800

    Site updated: 2025-03-14 16:23:32

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,2 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const themeToggle = document.querySelector('#color-toggle-btn'); // 夜间模式切换按钮的选择器
-  if (themeToggle) {
-    themeToggle.addEventListener('click', () => {
-      const isDarkMode = document.documentElement.getAttribute('data-user-color-scheme') === 'dark';
-      if (isDarkMode) {
-        document.documentElement.style.setProperty('--border-color', '#444');
-        document.documentElement.style.setProperty('--body-bg-color', '#222');
-        document.documentElement.style.setProperty('--primary-color', '#00ff80');
-      } else {
-        document.documentElement.style.setProperty('--border-color', '#ccc');
-        document.documentElement.style.setProperty('--body-bg-color', '#f9f9f9');
-        document.documentElement.style.setProperty('--primary-color', '#3498db');
-      }
-    });
-  }
-});
+<!--音乐播放器所用的文件-->
+<script src="https://cdn.staticfile.org/jquery/3.5.1/jquery.min.js"></script>
